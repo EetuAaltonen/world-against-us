26207da54f9e11b52409b16d5c205db0cceb632f
Eetu Aaltonen <eetu.aaltonen98@gmail.com>
2023-12-24 17:19:51 +0200

    Add constant MessageType values for 'sync scouting drone data' and 'scouting drone data position' on server. Add classes for 'scouting drone' and 'operations scout stream'

---
+++ nodejs-server/src/modules/operations_center/OperationsScoutStream.js
@@ -0,0 +1,7 @@
+export default class OperationsScoutStream {
+  constructor(instanceId, scoutingDrone, operatingClient) {
+    this.instanceId = instanceId;
+    this.scoutingDrone = scoutingDrone;
+    this.operatingClient = operatingClient;
+  }
+}
