788a644430c9dd67938275a104aaa7a76557bb30
Eetu Aaltonen <eetu.aaltonen98@gmail.com>
2024-03-08 21:27:22 +0200

    Fix 'sync patrol state' related network packet handling in NetworkPacketHandler class on server

--- nodejs-server/src/modules/network/NetworkPacketHandler.js
+++ nodejs-server/src/modules/network/NetworkPacketHandler.js
@@ -692,10 +692,18 @@ export default class NetworkPacketHandler {
             const patrolState = networkPacket.payload;
             if (patrolState !== undefined) {
               if (patrolState.instanceId === instance.instanceId) {
-                if (instance.handlePatrolState(patrolState)) {
-                  this.networkHandler.queueAcknowledgmentResponse(client);
+                if (instance.syncPatrolState(patrolState)) {
+                  // Broadcast about new patrol state within the instance
+                  this.networkHandler.broadcastPatrolState(
+                    instance.instanceId,
+                    patrolState,
+                    client.uuid
+                  );
+
+                  // Respond with acknowledgment
+                  isPacketHandled =
+                    this.networkHandler.queueAcknowledgmentResponse(client);
                 }
-                isPacketHandled = true;
               }
             }
           }
