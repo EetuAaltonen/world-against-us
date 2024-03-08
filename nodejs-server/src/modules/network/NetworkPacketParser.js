5a5f04672bf88658a6e0adc5415d2b9d931f790b
Eetu Aaltonen <eetu.aaltonen98@gmail.com>
2024-03-08 21:24:08 +0200

    Fix 'sync patrol state' related network packet parsing in NetworkPacketParser class on server

--- nodejs-server/src/modules/network/NetworkPacketParser.js
+++ nodejs-server/src/modules/network/NetworkPacketParser.js
@@ -299,10 +299,27 @@ export default class NetworkPacketParser {
               const parsedPatrolId = msg.readUInt8(offset);
               offset += BITWISE.BIT8;
               const parsedAIState = msg.readUInt8(offset);
+              offset += BITWISE.BIT8;
+              const parsedRouteProgress = msg.readFloatLE(offset);
+              offset += BITWISE.BIT32;
+              const parsedPositionX = msg.readUInt32LE(offset);
+              offset += BITWISE.BIT32;
+              const parsedPositionY = msg.readUInt32LE(offset);
+              offset += BITWISE.BIT32;
+              const parsedTargetNetworkId = msg.toString("utf8", offset);
+
+              const parsedPosition = new Vector2(
+                parsedPositionX,
+                parsedPositionY
+              );
+
               payload = new PatrolState(
                 parsedInstanceId,
                 parsedPatrolId,
-                parsedAIState
+                parsedAIState,
+                parsedRouteProgress,
+                parsedPosition,
+                parsedTargetNetworkId
               );
             }
             break;
